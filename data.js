0a4fd531d92f1bc8a0f2a653727ba0e1e8f25914
axiemaid <123000197+axiemaid@users.noreply.github.com>
2023-10-18 22:26:58 -0600

    Initial Commit
    Version 18

---
+++ data.js
@@ -0,0 +1,39 @@
+const tagsData = [
+    {
+        block: 0,
+        tags: ["bitoshi", "blockamoto", "genesis", "founder", "OG"],
+        detail: ["Bitmap Valley"]
+    },
+    {
+        block: 8,
+        tags: ["8bit", "8 bit", "versions", "recursion", "art", "OG"],
+        detail: ["Bitmap Valley"]
+    },
+    {
+        block: 91,
+        tags: ["Block", "Runner", "Iman", "Podcast", "YouTube"],
+        detail: ["Bitmap Valley"]
+    },
+    {
+        block: 92,
+        tags: ["Block", "Runner", "Iman", "Podcast", "YouTube"],
+        detail: ["Bitmap Valley"]
+    },
+    {
+        block: 96,
+        tags: ["Block", "Runner", "Iman", "Podcast", "YouTube"],
+        detail: ["Bitmap Valley"]
+    },
+    {
+        block: 100,
+        tags: ["Block", "Runner", "Iman", "Podcast", "YouTube"],
+        detail: ["Bitmap Valley"]
+    },
+    {
+        block: 505,
+        tags: ["axiemaid"],
+        detail: ["Bitmap Valley"]
+    },
+
+    // ... more entries ...
+];
